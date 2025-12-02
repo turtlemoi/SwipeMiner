e0725ad57ed6a33f0dd1935e4129e53ed147913a
hinaturtlegg <hinaturtlegg@gmail.com>
2025-12-02 21:44:48 +0900

    new project

---
+++ Assets/Scripts/Prototype/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(horizontal, 0f, vertical);
+    }
+}
